952720216a8597bbec655a22be7957b1057e0df6
Pavan Kalyan <86455380+PavanKalyan12087@users.noreply.github.com>
2022-04-09 15:50:36 +0530

    Message Changed

--- Sample.java
+++ Sample.java
@@ -2,6 +2,6 @@ Import java.io.*;
 Class Sample{
   public static void main(String[] args){
     System.out.println("Hello AWS Cloud");
-  System.out.println("Hello PK");}
+  System.out.println("New User");}
 }
 
