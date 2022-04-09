ddb4c11f11cad5b60a5f558829e414be6d751ef5
Mandala Pavan Kalyan <55622145+PavanKalyan2111@users.noreply.github.com>
2022-04-09 15:32:36 +0530

    New Message is Added

--- Sample.java
+++ Sample.java
@@ -1,6 +1,7 @@
 Import java.io.*;
 Class Sample{
   public static void main(String[] args){
-    System.out.println("Hello AWS Cloud");}
+    System.out.println("Hello AWS Cloud");
+  System.out.println("Hello PK");}
 }
 
